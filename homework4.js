1978510d781270cf52b8375419f007b95b7242d0
sajaalgadhi <saja.algadhi@outlook.com>
2019-01-16 19:57:47 +0300

    the hardest

---
+++ homework4.js
@@ -0,0 +1,16 @@
+\\\\\\\\\\\ exercise 1
+var test=9;	
+for(i=0 ; i<=10 ; i++)
+{ console.log(i*test);}
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ exercise2
+var grade = 99;for(var i=60 ; i<=100 ; i++)		                           
+{
+    if( grade <= 69){ 
+        console.log("For "+grade+" ,you get an D")} 												
+        else if(grade<=79){
+            console.log("For "+grade+" ,you get an C")}		
+            else if(grade<=89){
+                console.log("For "+grade+" ,you get an B")}	
+                else if(grade<=99){
+                    console.log("For "+grade+" ,you get an A")}}
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\exercise3
