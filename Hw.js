49af9dcb41abb84f4e1bebf679427aa937fbdac4
ANFAL <anfaljaferi@gmail.com>
2019-01-17 09:02:10 +0300

    Updated file

---
+++ Hw.js
@@ -0,0 +1,19 @@
+
+//Multiplication Tables
+var multiply = 9;
+for (var i=0; i<=10; i++ ){
+	console.log("2 * 9 = "+ i * multiply );
+}
+
+//The Grade Assigner
+for (var i=0; i<60; i++){
+    if (i<100){
+    console.log("Your grade is A")
+    } if (i<90){
+    console.log("Your grade is B")
+    } if (i<80){
+    console.log("Your grade is C")
+    } if (i<70) {
+    console.log("Your grade is D")
+    }
+    }
